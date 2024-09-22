da63bccce49a3cb2633e352501cf73296165e954
SAMAELGAMEZ <al02976026@tecmilenio.mx>
2024-09-22 00:03:32 -0600

    Evidencia 1_SQLScripts

---
+++ EV1-SQLScripts/Producto.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[Producto](
+	[idProducto] [int] IDENTITY(1,1) NOT NULL,
+	[nombreProducto] [varchar](100) NOT NULL,
+	[precio] [decimal](10, 2) NOT NULL,
+	[stock] [int] NOT NULL,
+ CONSTRAINT [PK_Producto] PRIMARY KEY CLUSTERED 
+(
+	[idProducto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
