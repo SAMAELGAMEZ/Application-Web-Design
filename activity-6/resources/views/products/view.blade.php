7c6f85a58942e5f6a0e7110185d959a00b067527
SAMAELGAMEZ <al02976026@tecmilenio.mx>
2024-09-27 18:51:45 -0600

    homework 5

--- activity-6/resources/views/products/view.blade.php
+++ activity-6/resources/views/products/view.blade.php
@@ -4,6 +4,11 @@
     <title>Product View</title>
 </head>
 <body>
+    <h1>Product List</h1>
+    
+    <a href="{{ route('products.create') }}">
+        <button>Add Product</button>
+    </a>
     <!--CONTENIDO-->
 </body>
 </html>
