4a6d9bcbcf7f753f68fd225bc7bef23d60290bf3
SAMAELGAMEZ <al02976026@tecmilenio.mx>
2024-10-01 00:42:55 -0600

    Proyecto de Activity 7 completado

---
+++ activity7/database/migrations/2024_10_01_061434_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('course_key');
+            $table->string('course_name');
+            $table->string('robotics_kit');
+            $table->string('course_cover')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
