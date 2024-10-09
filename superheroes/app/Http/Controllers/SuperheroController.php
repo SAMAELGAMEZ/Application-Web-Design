4d202ba04e4c58e0826431f0466312929105ea4b
SAMAEGAMEZ <al02976026@tecmilenio.mx>
2024-10-08 18:34:01 -0600

    Actividad8

---
+++ superheroes/app/Http/Controllers/SuperheroController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SuperheroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'real_name' => 'required|string|max:255',
+            'hero_name' => 'required|string|max:255',
+            'photo_url' => 'required|url',
+            'additional_info' => 'nullable|string',
+        ]);
+
+        Superhero::create([
+            'real_name' => $request->real_name,
+            'hero_name' => $request->hero_name,
+            'photo_url' => $request->photo_url,
+            'additional_info' => $request->additional_info,
+        ]);
+
+        return redirect()->route('superheroes.index')->with('success', 'Superhero created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
