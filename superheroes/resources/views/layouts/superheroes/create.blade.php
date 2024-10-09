7f98e77cef0bca7cd1cefd5efd7c4cdc411c4cdc
SAMAEGAMEZ <al02976026@tecmilenio.mx>
2024-10-08 19:56:27 -0600

    Activity8_Laravel

---
+++ superheroes/resources/views/layouts/superheroes/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app') 
+@section('content') 
+  <style> 
+    .uper { 
+      margin-top: 40px; 
+    } 
+  </style> 
+  <div class="card uper"> 
+    <div class="card-header"> 
+      New Superhero 
+    </div> 
+    <div class="card-body"> 
+      @if ($errors->any()) 
+        <div class="alert alert-danger"> 
+          <ul> 
+            @foreach ($errors->all() as $error) 
+              <li>{{ $error }}</li> 
+            @endforeach 
+          </ul> 
+        </div><br /> 
+      @endif 
+      <form method="post" action="{{ route('superheroes.store') }}"> 
+        @csrf 
+        <div class="form-group"> 
+          <label for="name">Superhero Name:</label> 
+          <input type="text" class="form-control" name="name" /> 
+        </div> 
+        <div class="form-group"> 
+          <label for="real_name">Real Name:</label> 
+          <input type="text" class="form-control" name="real_name" /> 
+        </div> 
+        <div class="form-group"> 
+          <label for="universe">Universe:</label> 
+          <input type="text" class="form-control" name="universe" /> 
+        </div> 
+        <button type="submit" class="btn btn-primary">Save</button> 
+        <a href="{{ route('superheroes.index') }}" class="btn btn-primary">Return</a> 
+      </form> 
+    </div> 
+  </div> 
+@endsection
--- superheroes/routes/web.php
+++ superheroes/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuperheroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('superheroes', SuperheroController::class);
+
+/*
+Route::get('superheroes', 'SuperheroController@index')->name('superheroes.index'); 
+Route::post('superheroes', 'SuperheroController@store')->name('superheroes.store'); 
+Route::post('superheroes/create', 'SuperheroController@create')->name('superheroes.create'); 
+Route::get('superheroes/{id}', 'SuperheroController@show')->name('superheroes.show'); 
+Route::patch('superheroes/{id}', 'SuperheroController@update')->name('superheroes.update'); 
+Route::delete('superheroes/{id}', 'SuperheroController@destroy')->name('superheroes.destroy'); 
+Route::get('superheroes/{id}/edit', 'SuperheroController@edit')->name('superheroes.edit'); 
+*/
